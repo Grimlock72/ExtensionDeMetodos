18365c24a2855ee4b7d383ccfd605ab6d9df5eb7
Juan Carlos Cuellar Ruz <juan-carlos.cuellar@atos.net>
2022-03-28 14:01:28 -0600

    El parámetro genérico (T) sólo permite clases derivadas.

---
+++ ExtensionDeMetodos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionDeMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World!".Map());
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Map(this string T) // this. Para indicarle al compilador qué tipo va a extender.
+        {
+            char firstLetter = char.ToUpper(T[0]);
+            string restPhrase = T.Substring(0);
+            return firstLetter + "\n\r" + restPhrase;
+        }
+    }
+}
